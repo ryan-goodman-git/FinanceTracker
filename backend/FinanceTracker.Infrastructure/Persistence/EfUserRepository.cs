d0c944c8f2ac234f2b68687b3056d477f00c2536
ryangoodman <ryan.123.goodman@gmail.com>
2026-04-27 12:17:48 +0100

    Implement EF user repository add, update, and aggregate loading methods

--- backend/FinanceTracker.Infrastructure/Persistence/EfUserRepository.cs
+++ backend/FinanceTracker.Infrastructure/Persistence/EfUserRepository.cs
@@ -1,5 +1,6 @@
 using FinanceTracker.Application.Interfaces;
 using FinanceTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceTracker.Infrastructure.Persistence;
 
@@ -14,16 +15,21 @@ public class EfUserRepository : IUserRepository
 
     public User? GetById(Guid userId)
     {
-        throw new NotImplementedException();
+        return _dbContext.Users
+            .Include(user => user.RecurringTransactions)
+            .Include(user => user.OneOffTransactions)
+            .SingleOrDefault(user => user.Id == userId);
     }
 
     public void Add(User user)
     {
-        throw new NotImplementedException();
+        _dbContext.Users.Add(user);
+        _dbContext.SaveChanges();
     }
 
     public void Update(User user)
     {
-        throw new NotImplementedException();
+        _dbContext.Users.Update(user);
+        _dbContext.SaveChanges();
     }
 }
