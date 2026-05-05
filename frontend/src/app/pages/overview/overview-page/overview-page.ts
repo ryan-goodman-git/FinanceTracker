448f3fcacf7fd8b9eaed2267df316530d6c05bc9
ryangoodman <ryan.123.goodman@gmail.com>
2026-05-05 18:20:40 +0100

    Set up Angular frontend with initial routing

---
+++ frontend/src/app/pages/overview/overview-page/overview-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-page',
+  imports: [],
+  templateUrl: './overview-page.html',
+  styleUrl: './overview-page.scss',
+})
+export class OverviewPage {}
---
+++ frontend/src/app/pages/users/users-page/users-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-page',
+  imports: [],
+  templateUrl: './users-page.html',
+  styleUrl: './users-page.scss',
+})
+export class UsersPage {}
